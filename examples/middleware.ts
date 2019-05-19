5a9611f23b2fcc811fb3ca8710f152435fa20186
Athyuttam Eleti <atty@stripe.com>
2019-05-19 13:29:56 -0700

    Add wip combineMiddleware

--- examples/middleware.ts
+++ examples/middleware.ts
@@ -1,7 +1,12 @@
+import { Context } from "../core/context.ts";
 import { Middleware } from "../core/middleware.ts";
 
 type Extension = { user: string };
-const setUser: Middleware<Extension> = async (request, context, next) => {
+const setUser: Middleware<Context, Extension> = async (
+  request,
+  context,
+  next
+) => {
   const contextWithUser = { ...context, user: "user_123" };
   return await next(request, contextWithUser);
 };
